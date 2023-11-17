d00f8398bff97953ca29ddc026d99c8db6b789bf
wangzijie <1404081737@qq.com>
2023-11-17 18:00:53 +0800

    1

--- vue-manager/src/page/FirstPage.vue
+++ vue-manager/src/page/FirstPage.vue
@@ -3,11 +3,11 @@
     <h1>数据统计</h1>
     <div class="static-input">
       <!--   -->
-      <span>当日数据</span> <span>{{ formInfo.newUser }}新增用户</span>
-      <span>>{{ formInfo.newOrder }}新增订单</span><span>>{{ formInfo.newManager }}新增管理员</span>
+      <span style="background-color: orange;" class="title1">当日数据</span> <span>{{ formInfo.newUser }}新增用户</span>
+      <span>{{ formInfo.newOrder }}新增订单</span><span>{{ formInfo.newManager }}新增管理员</span>
     </div>
     <div class="static-input">
-      <span>总数据</span> <span>{{ formInfo.totalUser }}注册用户</span>
+      <span style="background-color: steelblue;" class="title2">总数据</span> <span>{{ formInfo.totalUser }}注册用户</span>
       <span>{{ formInfo.totalUser }}订单</span><span>{{ formInfo.totalManager }}管理员</span>
     </div>
   </div>
@@ -33,10 +33,11 @@ export default {
 
   },
   created() {
-    var res =  dataStatic({ "time": "2023-12-12 10:10" })
-    console.log("111"+res)
-    
-    res.then(((dat)=>{
+    var res = dataStatic({ "time": "2023-12-12 10:10" })
+    //
+    console.log(res)
+    res.then(((dat) => {
+      console.log("111" + res)
       this.$data.formInfo = dat;
     }))
   }
@@ -44,7 +45,30 @@ export default {
 </script>
 
 <style>
-.span {
+h1 {
+  padding-left: 400px;
+}
+
+.static-input {
+  margin: 30px auto;
+}
+
+.static-input span {
   display: inline-block;
+  width: 200px;
+  height: 40px;
+  /* border:black solid 1px ; */
+  background-color: rgb(209, 206, 206);
+  margin: auto 20px;
+  border-radius: 8px;
+  line-height: 40px;
+}
+
+.title1 {
+  background-color: orange;
+}
+
+.title2 {
+  background-color: steelblue;
 }
 </style>
