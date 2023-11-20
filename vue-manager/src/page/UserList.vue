5ca2bf0ae3a1e5832eafe7c9102e50e84f04e262
wangzijie <1404081737@qq.com>
2023-11-20 17:43:59 +0800

    1

--- vue-manager/src/page/UserList.vue
+++ vue-manager/src/page/UserList.vue
@@ -1,48 +1,31 @@
 <template>
-
   <div>
     <el-table ref="singleTable" :data="tableData" highlight-current-row @current-change="handleCurrentChange"
-    style="width: 100%">
-    <el-table-column property="id" label="id" width="120">
-    </el-table-column>
-    <el-table-column property="date" label="日期" width="120">
-    </el-table-column>
-    <el-table-column property="name" label="姓名" width="120">
-    </el-table-column>
-    <el-table-column property="area" label="地址">
-    </el-table-column>
-  </el-table>
-  <el-pagination background layout="prev, pager, next" :page-sizes="[10,20,30]"	  :total="100">
+      style="width: 100%">
+      <el-table-column property="id" label="id" width="120">
+      </el-table-column>
+      <el-table-column property="date" label="日期" width="220">
+      </el-table-column>
+      <el-table-column property="name" label="姓名" width="120">
+      </el-table-column>
+      <el-table-column property="area" label="地址">
+      </el-table-column>
+    </el-table>
+    <el-pagination ref="paginationNum" background layout="prev, pager, next" :page-sizes="[10, 20, 30]" :total="totalCount">
     </el-pagination>
   </div>
 </template>
 
 <script>
+import { userList } from '@/api/getData.js'
 export default {
   data() {
     return {
-      tableData: [{
-        date: '2016-05-02',
-        name: '王小虎',
-        address: '上海市普陀区金沙江路 1518 弄'
-      }, {
-        date: '2016-05-04',
-        name: '王小虎',
-        address: '上海市普陀区金沙江路 1517 弄'
-      }, {
-        date: '2016-05-01',
-        name: '王小虎',
-        address: '上海市普陀区金沙江路 1519 弄'
-      }, {
-        date: '2016-05-03',
-        name: '王小虎',
-        address: '上海市普陀区金沙江路 1516 弄'
-      }],
-      currentRow: null
+      tableData: null,
+      currentRow: null,
+      totalCount: 10
     }
   },
-
-
 
   methods: {
     setCurrent(row) {
@@ -51,10 +34,21 @@ export default {
     handleCurrentChange(val) {
       this.currentRow = val;
     },
-    queryData(){
-      //
+    queryData(page,pageSize) {
+      var res = userList({ "page": page,"pageSize": pageSize })
 
+      res.then(((dat) => {
+        console.log( dat)
+        this.$data.tableData = dat.data;
+        this.$data.totalCount =dat.total
+      }))
+
+    },
+
+  },
+  created() {
+      console.log(123)
+      this.queryData(1,10)
     }
-  }
 }
 </script>
