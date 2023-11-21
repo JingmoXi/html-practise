fd179fe0937662aa373a32b4938f9dea47073571
wangzijie <1404081737@qq.com>
2023-11-21 16:01:20 +0800

    1

--- vue-manager/src/page/ManagePage.vue
+++ vue-manager/src/page/ManagePage.vue
@@ -3,41 +3,42 @@
     <el-container style="height: 100%; border: 1px solid #eee">
       <el-aside width="200px" style="background-color: rgb(50, 64, 87)">
         <!-- 左侧菜单 -->
-        <el-menu :default-openeds="['1', '3']" default-active="1"  active-text-color="#20a0ff" text-color="#bfcbd9" background-color="#324157"
+        <el-menu :default-active="defaultactive"  :unique-opened="uniqueopened" active-text-color="#ffd04b"
+          text-color="#bfcbd9" background-color="#324157" @select="handleSelect" @open="handleOpen" @close="handleClose"
           router>
-          <router-link to="/manage/firstPage"><el-menu-item index="1" route="firstPage" @click="menuChange(1)"><i
+          <router-link to="/manage/firstPage"><el-menu-item index="1"  ><i
                 class="el-icon-message"></i>首页</el-menu-item></router-link>
           <el-submenu index="2">
             <template slot="title"><i class="el-icon-message"></i>数据管理</template>
-            <router-link to="/manage/userList"><el-menu-item index="2-1"
-                style="color: #bfcbd9;">用户列表</el-menu-item></router-link>
-            <router-link to="/manage/userList"><el-menu-item index="2-2" style="color: #bfcbd9;"
-                route="userList">商家列表</el-menu-item></router-link>
+            <router-link to="/manage/userList"><el-menu-item index="2-1" ref="userlist"
+                >用户列表</el-menu-item></router-link>
+            <router-link to="/manage/userList"><el-menu-item index="2-2"
+                >商家列表</el-menu-item></router-link>
             <router-link to="/manage/userList"><el-menu-item index="2-3"
-                style="color: #bfcbd9;">选项3</el-menu-item></router-link>
+                >选项3</el-menu-item></router-link>
           </el-submenu>
-          <el-submenu index="3">
+          <el-submenu index="3" >
             <template slot="title"><i class="el-icon-message"></i>添加数据</template>
-            <el-menu-item index="3-1" style="color: #bfcbd9;">添加商铺</el-menu-item>
-            <el-menu-item index="3-2" style="color: #bfcbd9;">添加商品</el-menu-item>
+            <el-menu-item ref="shangpu" index="3-1"  >添加商铺</el-menu-item>
+            <el-menu-item index="3-2" >添加商品</el-menu-item>
 
           </el-submenu>
           <el-submenu index="4">
             <template slot="title"><i class="el-icon-message"></i>图标</template>
-            <el-menu-item index="4-1" style="color: #bfcbd9;">用户分布</el-menu-item>
+            <el-menu-item index="4-1" >用户分布</el-menu-item>
           </el-submenu>
           <el-submenu index="5">
             <template slot="title"><i class="el-icon-message"></i>编辑</template>
-            <el-menu-item index="5-1" style="color: #bfcbd9;">文本编辑</el-menu-item>
+            <el-menu-item index="5-1" >文本编辑</el-menu-item>
           </el-submenu>
 
           <el-submenu index="6">
             <template slot="title"><i class="el-icon-message"></i>设置</template>
-            <el-menu-item index="6-1" style="color: #bfcbd9;">管理员设置</el-menu-item>
+            <el-menu-item index="6-1" >管理员设置</el-menu-item>
           </el-submenu>
           <el-submenu index="6">
             <template slot="title"><i class="el-icon-message"></i>说明</template>
-            <el-menu-item index="6-1" style="color: #bfcbd9;">说明</el-menu-item>
+            <el-menu-item index="6-1" >说明</el-menu-item>
           </el-submenu>
 
         </el-menu>
@@ -60,27 +61,36 @@
 
 <script>
 
-// import PageHead from '@/components/PageHead.vue'
+// import PageHead from '@/components/PageHead.vue' 
 
 export default {
   data() {
-    const item = {
-      date: '2016-05-02',
-      name: '王小虎',
-      address: '上海市普陀区金沙江路 1518 弄'
-    };
     return {
-      tableData: Array(5).fill(item)
+      uniqueopened: true,
+      colll: '#20a0ff',
+      defaultactive: "2-1",
+      defaultopeneds: ['2']
     }
   },
   methods: {
-    menuChange(index) {
-      console.log(index)
-      // if (index == 1) {
-      //   this.$router.push('manage')
-      // } else {
-      //   this.$router.push('manage')
-      // }
+
+    handleSelect(key, keyPath) {
+      console.log("handleSelect")
+      console.log(key)
+      console.log(keyPath)
+      this.$data.defaultactive="2-2"
+      console.log(this.$data)
+      console.log("handleSelect")
+    },
+    handleOpen(key, keyPath) {
+      console.log("handleOpen")
+      console.log(key);
+      this.$data.defaultopeneds=keyPath
+      
+    },
+    handleClose(key, keyPath) {
+      console.log("handleClose")
+      console.log(key, keyPath);
     }
   },
   components: {
@@ -129,8 +139,9 @@ export default {
 .el-icon-message {
   color: rgb(50, 64, 87);
 }
-.router-link-active {
-  background-color: blue; /* 或者你喜欢的任何颜色 */
-}
 
+.router-link-active {
+  background-color: blue;
+  /* 或者你喜欢的任何颜色 */
+}
 </style>
---
+++ vue-manager/src/page/SellePage.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>
+        <el-table ref="singleTable" :data="tableData" highlight-current-row @current-change="handleCurrentChange"
+            style="width: 100%">
+            <el-table-column property="id" label="" width="120">
+            </el-table-column>
+            <el-table-column property="date" label="店铺名称" width="220">
+            </el-table-column>
+            <el-table-column property="name" label="店铺地址" width="120">
+            </el-table-column>
+            <el-table-column property="area" label="店铺介绍">
+            </el-table-column>
+            <el-table-column fixed="right" label="操作" width="100">
+                <template slot-scope="scope">
+                    <el-button @click="handleClick(scope.row)" type="text" size="small">查看</el-button>
+                    <el-button type="text" size="small">编辑</el-button>
+                </template>
+            </el-table-column>
+        </el-table>
+        <el-pagination ref="paginationNum" @current-change="handleCurrentChange" background layout="prev, pager, next"
+            :page-size="pagesize" :page-sizes="pagesizes" :current-page="currentPage" :total="totalCount">
+        </el-pagination>
+    </div>
+</template>
+
+<script>
+export default {
+
+    //
+}
+</script>
+
+<style></style>
